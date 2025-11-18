16c1581a140bdf7bd11becca9bf88b5f7163fba9
tesfay haile <tesfayhailemariam29@gmail.com>
2025-11-18 22:04:01 +0300

    Add LoginSignup component

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -6,7 +6,44 @@ import email_icon from '../Assets/email.png';
 import password_icon from '../Assets/password.png';
 const LoginSignup = () => {
     const [action, setAction] = useState("Sign up");
+    
+    // state to store input values
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    // ---------- SIGNUP FUNCTION ----------
+    const handleSignup = () => {
+        axios.post("http://127.0.0.1:8000/api/auth/register/", {
+            name: name,
+            email: email,
+            password: password
+        })
+        .then(res => {
+            alert("Signup successful!");
+            console.log(res.data);
+        })
+        .catch(err => {
+            alert("Signup failed!");
+            console.log(err);
+        });
+    };
+
+    // ---------- LOGIN FUNCTION ----------
+    const handleLogin = () => {
+        axios.post("http://127.0.0.1:8000/api/auth/login/", {
+            email: email,
+            password: password
+        })
+        .then(res => {
+            alert("Login successful!");
+            console.log(res.data);
+        })
+        .catch(err => {
+            alert("Login failed!");
+            console.log(err);
+        });
+    };
     return (
         <div className='container'>
             <div className="header">
@@ -18,16 +55,16 @@ const LoginSignup = () => {
                 {action === "Login" ? null : (
                     <div className="input">
                         <img src={user_icon} alt="user"/>
-                        <input type="text" placeholder="Enter your name"/>
+                        <input type="text" placeholder="Enter your name" onChange={(e) => setName(e.target.value)}/>
                     </div>
                 )}
                 <div className="input">
                     <img src={email_icon} alt="email"/>
-                    <input type="email" placeholder="Enter email"/>
+                    <input type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div className="input">
                     <img src={password_icon} alt="password"/>
-                    <input type="password" placeholder="Enter password"/>
+                    <input type="password" placeholder="Enter password" onChange={(e) => setPassword(e.target.value)}/>
                 </div>
             </div>
 
@@ -46,7 +83,7 @@ const LoginSignup = () => {
                 </div>
                 <div 
                     className={action === "Login" ? "submit gray" : "submit"} 
-                    onClick={() => setAction("Login")}ihkhk
+                    onClick={() => setAction("Login")}
                 >
                     Login
                 </div>
